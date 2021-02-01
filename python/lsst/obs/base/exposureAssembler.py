gcmshadow/obs_base

Add support for reading and dis/assembling ExposureSummary.
The only change to the test calexp.fits is the addition of a SUMMARY
component.  The rest of the file remains unchanged.

python/lsst/obs/base/exposureAssembler.py
@@ -33,7 +33,7 @@ class ExposureAssembler(StorageClassDelegate):
     EXPOSURE_COMPONENTS = set(("image", "variance", "mask", "wcs", "psf"))
     EXPOSURE_INFO_COMPONENTS = set(("apCorrMap", "coaddInputs", "photoCalib", "metadata",
                                     "filterLabel", "transmissionCurve", "visitInfo",
-                                    "detector", "validPolygon"))
+                                    "detector", "validPolygon", "summary"))
     EXPOSURE_READ_COMPONENTS = {"bbox", "dimensions", "xy0", "filter"}
 
     COMPONENT_MAP = {"bbox": "BBox", "xy0": "XY0"}
@@ -228,6 +228,7 @@ class ExposureAssembler(StorageClassDelegate):
         info.setValidPolygon(components.pop("validPolygon", None))
         info.setDetector(components.pop("detector", None))
         info.setTransmissionCurve(components.pop("transmissionCurve", None))
+        info.setSummary(components.pop("summary", None))
 
         # TODO: switch back to "filter" as primary component in DM-27177
         info.setFilterLabel(components.pop("filterLabel", None))

tests/test_butlerFits.py
@@ -77,7 +77,7 @@ datastore:
 
 # Components present in the test file
 COMPONENTS = {"wcs", "image", "mask", "coaddInputs", "psf", "visitInfo", "variance", "metadata", "photoCalib",
-              "filterLabel", "validPolygon", "transmissionCurve", "detector", "apCorrMap"}
+              "filterLabel", "validPolygon", "transmissionCurve", "detector", "apCorrMap", "summary"}
 READ_COMPONENTS = {"bbox", "xy0", "dimensions", "filter"}
 
 
@@ -307,6 +307,8 @@ class ButlerFitsTests(DatasetTestHelper, lsst.utils.tests.TestCase):
                 c_amps = {a.getName() for a in component.getAmplifiers()}
                 r_amps = {a.getName() for a in reference.getAmplifiers()}
                 self.assertEqual(c_amps, r_amps)
+            elif compName == 'summary':
+                self.assertEqual(component.psfSigma, reference.psfSigma)
             else:
                 raise RuntimeError(f"Unexpected component '{compName}' encountered in test")
 

lsst-dm/daf_butlerUtils

python/lsst/obs/base/exposureAssembler.py
@@ -33,7 +33,7 @@ class ExposureAssembler(StorageClassDelegate):
     EXPOSURE_COMPONENTS = set(("image", "variance", "mask", "wcs", "psf"))
     EXPOSURE_INFO_COMPONENTS = set(("apCorrMap", "coaddInputs", "photoCalib", "metadata",
                                     "filterLabel", "transmissionCurve", "visitInfo",
-                                    "detector", "validPolygon"))
+                                    "detector", "validPolygon", "summaryStats"))
     EXPOSURE_READ_COMPONENTS = {"bbox", "dimensions", "xy0", "filter"}
 
     COMPONENT_MAP = {"bbox": "BBox", "xy0": "XY0"}
@@ -228,6 +228,7 @@ class ExposureAssembler(StorageClassDelegate):
         info.setValidPolygon(components.pop("validPolygon", None))
         info.setDetector(components.pop("detector", None))
         info.setTransmissionCurve(components.pop("transmissionCurve", None))
+        info.setSummaryStats(components.pop("summaryStats", None))
 
         # TODO: switch back to "filter" as primary component in DM-27177
         info.setFilterLabel(components.pop("filterLabel", None))

tests/test_butlerFits.py
@@ -77,7 +77,7 @@ datastore:
 
 # Components present in the test file
 COMPONENTS = {"wcs", "image", "mask", "coaddInputs", "psf", "visitInfo", "variance", "metadata", "photoCalib",
-              "filterLabel", "validPolygon", "transmissionCurve", "detector", "apCorrMap"}
+              "filterLabel", "validPolygon", "transmissionCurve", "detector", "apCorrMap", "summaryStats"}
 READ_COMPONENTS = {"bbox", "xy0", "dimensions", "filter"}
 
 
@@ -307,6 +307,8 @@ class ButlerFitsTests(DatasetTestHelper, lsst.utils.tests.TestCase):
                 c_amps = {a.getName() for a in component.getAmplifiers()}
                 r_amps = {a.getName() for a in reference.getAmplifiers()}
                 self.assertEqual(c_amps, r_amps)
+            elif compName == 'summaryStats':
+                self.assertEqual(component.psfSigma, reference.psfSigma)
             else:
                 raise RuntimeError(f"Unexpected component '{compName}' encountered in test")
 